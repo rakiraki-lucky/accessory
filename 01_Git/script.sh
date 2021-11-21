3ae4c33372fe935ac0901c4f6af8563fc4760515
rakiraki_lucky <galuckta.prototype.jt@gmail.com>
2021-11-21 20:56:46 +0900

    暫定版

--- 01_Git/script.sh
+++ 01_Git/script.sh
@@ -1,48 +1,109 @@
 #!/bin/bash
 
 ###
-### create path
+### add path
 ###
 
-COMMENT="### written by https://github.com/rakiraki-lucky/accessory"
-BASHRC_CONTENT=$(cat 'target1')
-SCRIPT_DIR_PATH=$(cd $(dirname $0); pwd)
-CMD="source "
+add_path(){
+  # $1: comment sentence
+  # $2: resource_dir_path 
+  # $3: target file path
 
-### setting .bashrc
-if [[ $BASHRC_CONTENT != *"${COMMENT}"* ]]
-then
-  TARGET_FILE=~/.bashrc
-  echo "start setting .bashrc..."
-  echo -e "\\n${COMMENT}" | cat >> TARGET_FILE
+  CMD="source "
+  RESOURCE_FILES=$(ls $2)
 
-  TO_BASHRC_DIR_PATH=$(cd "${SCRIPT_DIR_PATH}/to_bashrc/"; pwd)
-  TO_BASHRC_FILES=$(ls "${SCRIPT_DIR_PATH}/to_bashrc/" )
-
-  echo $TO_BASHRC_FILES | 
-    awk -v cmd="${CMD}" -v to_bashrc_dir_path="${TO_BASHRC_DIR_PATH}" '{ 
+  echo -e "\\n${1}" | cat >> $3
+  echo $RESOURCE_FILES | 
+    awk -v cmd="${CMD}" -v resource_dir_path="${RESOURCE_DIR_PATH}" '{ 
       for (i=1; i<=NF; i++) 
-        print cmd to_bashrc_dir_path "/" $i }' | cat >> TARGET_FILE
+        print cmd to_bashrc_dir_path "/" $i }' | cat >> $3
+}
+
+print_header(){
+  echo "======== \"$1\" settings ==================================="
+}
+
+
+COMMENT="###_written_by_https://github.com/rakiraki-lucky/accessory"
+SCRIPT_DIR_PATH=$(cd $(dirname $0); pwd)
+
+###
+### .bashrc settings
+###
+
+TARGET_PATH=$(ls ~/.bashrc)         
+RESOURCE_DIR_NAME="to_bashrc"
+RESOURCE_DIR_PATH="${SCRIPT_DIR_PATH}/${RESOURCE_DIR_NAME}"
+
+print_header $TARGET_PATH
+if [ -e $TARGET_PATH ]
+then
+  echo "\"${TARGET_PATH}\" exists."
+
+  CONTENT=$(cat $TARGET_PATH)
+  if [[ $CONTENT != *"${COMMENT}"* ]]
+  # judeg whether target file include comment sentence
+  # -> Yes)abort, No)execute
+  then
+    echo "\"${TARGET_PATH}\" does not set yet."
+
+    read -p "Do you execute setting to \"${TARGET_PATH}\"? (Y/n): " YN
+    case "${YN}" in
+      [yY]*)
+        echo "OK. Execute setting..."
+
+        add_path $COMMENT $RESOURCE_DIR_PATH $TARGET_PATH 
+        echo "Successfuly. Settings Done."
+        ;;
+      *)
+        echo "Abort." 
+        ;;
+    esac
+  else
+    echo "\"${TARGET_PATH}\" is already set."
+  fi
 else
-  echo ".bashrc already set"
+  echo "\"${TARGET_PATH}\" does not exist."
 fi
 
-BASHRC_CONTENT=$(cat 'target2')
+###
+### .gitconfig settings
+###
 
-### setting .gitconfig
-if [[ $BASHRC_CONTENT != *"${COMMENT}"* ]]
+TARGET_PATH=$(ls ~/.gitconfig)       
+RESOURCE_DIR_NAME="to_gitconfig"
+RESOURCE_DIR_PATH="${SCRIPT_DIR_PATH}/${RESOURCE_DIR_NAME}"
+
+#TARGET_PATH=~/.gitconfig
+
+print_header $TARGET_PATH
+if [ -e $TARGET_PATH ]
 then
-  TARGET_FILE=~/.gitconfig
-  echo "start setting .gitconfig..."
-  echo -e "\\n${COMMENT}" | cat >> target2
+  echo "\"${TARGET_PATH}\" exists."
 
-  TO_GITCONFIG_DIR_PATH=$(cd "${SCRIPT_DIR_PATH}/to_gitconfig/"; pwd)
-  TO_GITCONFIG_FILES=$(ls "${SCRIPT_DIR_PATH}/to_gitconfig/" )
+  CONTENT=$(cat $TARGET_PATH)
+  if [[ $CONTENT != *"${COMMENT}"* ]]
+  # judeg whether target file include comment sentence
+  # -> Yes)abort, No)execute
+  then
+    echo "\"${TARGET_PATH}\" does not set yet."
 
-  echo $TO_GITCONFIG_FILES | 
-    awk -v cmd="${CMD}" -v to_gitconfig_dir_path="${TO_GITCONFIG_DIR_PATH}" '{ 
-      for (i=1; i<=NF; i++) 
-        print cmd to_gitconfig_dir_path "/" $i }' | cat >> target2
+    read -p "Do you execute setting to \"${TARGET_PATH}\"? (Y/n): " YN
+    case "${YN}" in
+      [yY]*)
+        echo "OK. Execute setting..."
+
+        add_path $COMMENT $RESOURCE_DIR_PATH $TARGET_PATH 
+        echo "Successfuly. Settings Done."
+        ;;
+      *)
+        echo "Abort." 
+        ;;
+    esac
+  else
+    echo "\"${TARGET_PATH}\" is already set."
+    echo "exit 0"
+  fi
 else
-  echo ".gitconfig already set"
+  echo "\"${TARGET_PATH}\" does not exist."
 fi
